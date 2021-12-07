036e9835a00bbc75b56eb7b00678b152145944a5
Eetu Jokinen <eetu.jokinen@tuni.fi>
2021-12-07 19:22:28 +0200

    Tasks  1-4 completed

--- src/main/scala/assignment21/assignment.scala
+++ src/main/scala/assignment21/assignment.scala
@@ -12,7 +12,7 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.{ArrayType, StringType, StructField, IntegerType, DoubleType}
 import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.functions.{count, sum, min, max, asc, desc, udf, to_date, avg}
+import org.apache.spark.sql.functions.{count, sum, min, max, asc, desc, udf, to_date, avg, when}
 
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.functions.array
@@ -39,6 +39,10 @@ import sys.process._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection.immutable.Range
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ForEach
+
+import scala.util.Sorting
+
 
 object assignment  {
   // Suppress the log messages:
@@ -71,6 +75,11 @@ object assignment  {
                        .option("header", "true")
                        .schema(schema2)
                        .csv("data/dataK5D3.csv")
+                       
+  val dataK5D3WithLabels = dataK5D2.withColumn("num(LABEL)", when(dataK5D2("LABEL") === " Ok", 0)
+                                    .otherwise(1))
+                                    
+  dataK5D3WithLabels.show
 
 
   def task1(df: DataFrame, k: Int): Array[(Double, Double)] = {
@@ -102,7 +111,7 @@ object assignment  {
     
     kmModel.summary.predictions.show
     
-    return kmModel.clusterCenters.map(vectorElement => (vectorElement(0), vectorElement(1)))
+    kmModel.clusterCenters.map(vectorElement => (vectorElement(0), vectorElement(1)))
     
   }
 
@@ -135,19 +144,82 @@ object assignment  {
     
     kmModel.summary.predictions.show
     
-    return kmModel.clusterCenters.map(vectorElement => (vectorElement(0), vectorElement(1), vectorElement(2)))
+    kmModel.clusterCenters.map(vectorElement => (vectorElement(0), vectorElement(1), vectorElement(2)))
   }
 
   def task3(df: DataFrame, k: Int): Array[(Double, Double)] = {
-    ???
+    
+    val vectorAssembler = new VectorAssembler()
+      .setInputCols(Array("a", "b", "num(LABEL)"))
+      .setOutputCol("unscaledFeatures")
+    
+    val transformationPipeline = new Pipeline()
+      .setStages(Array(vectorAssembler))
+    
+    //Fit produces a transformer
+    val pipeLine = transformationPipeline.fit(df)
+    val transformedData = pipeLine.transform(df)
+    transformedData.show
+    
+    val scaler = new MinMaxScaler()
+      .setInputCol("unscaledFeatures")
+      .setOutputCol("features")
+    
+    val scalerModel = scaler.fit(transformedData)
+    
+    val scaledData = scalerModel.transform(transformedData)
+    
+    val kmeans = new KMeans()
+      .setK(k)
+      .setSeed(1L)
+    
+    val kmModel = kmeans.fit(scaledData)
+    
+    kmModel.summary.predictions.show
+    
+    kmModel.clusterCenters
+      .sortWith((vectorElement1, vectorElement2)=>vectorElement1(2) > vectorElement2(2))
+      .map(vectorElement => (vectorElement(0), vectorElement(1)))
+      .take(2)
+    
   }
 
   // Parameter low is the lowest k and high is the highest one.
   def task4(df: DataFrame, low: Int, high: Int): Array[(Int, Double)]  = {
-    ???
+    val vectorAssembler = new VectorAssembler()
+      .setInputCols(Array("a", "b"))
+      .setOutputCol("unscaledFeatures")
+    
+    val transformationPipeline = new Pipeline()
+      .setStages(Array(vectorAssembler))
+    
+    //Fit produces a transformer
+    val pipeLine = transformationPipeline.fit(df)
+    val transformedData = pipeLine.transform(df)
+    transformedData.show
+    
+    val scaler = new MinMaxScaler()
+      .setInputCol("unscaledFeatures")
+      .setOutputCol("features")
+    
+    val scalerModel = scaler.fit(transformedData)
+    
+    val scaledData = scalerModel.transform(transformedData)
+    
+    var arrayOfCosts: Array[(Int, Double)] = Array()
+    
+    for ( k <- low to high){
+    
+      val kmeans = new KMeans()
+        .setK(k)
+        .setSeed(1L)
+    
+      val kmModel = kmeans.fit(scaledData)
+      val cost = kmModel.computeCost(scaledData)
+      arrayOfCosts +:= (k, cost)
+    }
+    return arrayOfCosts
   }
-
-
 
 }
 
